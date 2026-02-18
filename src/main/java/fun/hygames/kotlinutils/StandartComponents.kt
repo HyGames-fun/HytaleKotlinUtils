67bf37231c1f457b268650f2319ba33123255ff8
Lion <LionPa2009@gmail.com>
2026-02-18 20:34:17 +0700

    Added new standard components

--- src/main/java/fun/hygames/kotlinutils/StandartComponents.kt
+++ src/main/java/fun/hygames/kotlinutils/StandartComponents.kt
@@ -1,11 +1,36 @@
 package `fun`.hygames.kotlinutils
 
+import com.hypixel.hytale.server.core.entity.Frozen
+import com.hypixel.hytale.server.core.entity.UUIDComponent
 import com.hypixel.hytale.server.core.entity.effect.EffectControllerComponent
+import com.hypixel.hytale.server.core.entity.entities.BlockEntity
 import com.hypixel.hytale.server.core.entity.entities.Player
+import com.hypixel.hytale.server.core.entity.entities.ProjectileComponent
+import com.hypixel.hytale.server.core.entity.entities.player.CameraManager
+import com.hypixel.hytale.server.core.entity.entities.player.movement.MovementManager
+import com.hypixel.hytale.server.core.entity.knockback.KnockbackComponent
+import com.hypixel.hytale.server.core.entity.movement.MovementStatesComponent
+import com.hypixel.hytale.server.core.modules.entity.component.BoundingBox
+import com.hypixel.hytale.server.core.modules.entity.component.DisplayNameComponent
+import com.hypixel.hytale.server.core.modules.entity.component.DynamicLight
+import com.hypixel.hytale.server.core.modules.entity.component.EntityScaleComponent
+import com.hypixel.hytale.server.core.modules.entity.component.HeadRotation
+import com.hypixel.hytale.server.core.modules.entity.component.Invulnerable
+import com.hypixel.hytale.server.core.modules.entity.component.ModelComponent
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent
+import com.hypixel.hytale.server.core.modules.entity.hitboxcollision.HitboxCollision
+import com.hypixel.hytale.server.core.modules.entity.item.ItemComponent
+import com.hypixel.hytale.server.core.modules.entity.item.PickupItemComponent
+import com.hypixel.hytale.server.core.modules.entity.player.ChunkTracker
+import com.hypixel.hytale.server.core.modules.entity.player.KnockbackSimulation
+import com.hypixel.hytale.server.core.modules.entity.player.PlayerInput
+import com.hypixel.hytale.server.core.modules.entity.player.PlayerSettings
+import com.hypixel.hytale.server.core.modules.entity.player.PlayerSkinComponent
 import com.hypixel.hytale.server.core.modules.entity.teleport.Teleport
 import com.hypixel.hytale.server.core.modules.entitystats.EntityStatMap
 import com.hypixel.hytale.server.core.modules.entitystats.asset.DefaultEntityStatTypes
+import com.hypixel.hytale.server.core.modules.physics.component.PhysicsValues
+import com.hypixel.hytale.server.core.modules.physics.component.Velocity
 
 val TRANSFORM
     inline get() = TransformComponent.getComponentType()
@@ -69,3 +94,53 @@ val TELEPORT
     inline get() = Teleport.getComponentType()
 val EFFECT_CONTROLLER
     inline get() = EffectControllerComponent.getComponentType()
+val MODEL
+    inline get() = ModelComponent.getComponentType()!!
+val BOUNDING_BOX
+    inline get() = BoundingBox.getComponentType()!!
+val DYNAMIC_LIGHT
+    inline get() = DynamicLight.getComponentType()!!
+val ENTITY_SCALE
+    inline get() = EntityScaleComponent.getComponentType()!!
+val PLAYER_SKIN
+    inline get() = PlayerSkinComponent.getComponentType()
+val PLAYER_SETTINGS
+    inline get() = PlayerSettings.getComponentType()
+val PLAYER_INPUT
+    inline get() = PlayerInput.getComponentType()!!
+val KNOCKBACK_SIMULATION
+    inline get() = KnockbackSimulation.getComponentType()!!
+val CHUNK_TRACKER
+    inline get() = ChunkTracker.getComponentType()!!
+val ITEM
+    inline get() = ItemComponent.getComponentType()
+val PICKUP_ITEM
+    inline get() = PickupItemComponent.getComponentType()
+val HITBOX_COLLISION
+    inline get() = HitboxCollision.getComponentType()!!
+val UUID_COMPONENT
+    inline get() = UUIDComponent.getComponentType()
+val HEAD_ROTATION
+    inline get() = HeadRotation.getComponentType()!!
+val MOVEMENT_MANAGER
+    inline get() = MovementManager.getComponentType()!!
+val CAMERA_MANAGER
+    inline get() = CameraManager.getComponentType()!!
+val FROZEN
+    inline get() = Frozen.getComponentType()!!
+val PROJECTILE
+    inline get() = ProjectileComponent.getComponentType()
+val BLOCK_ENTITY
+    inline get() = BlockEntity.getComponentType()!!
+val DISPLAY_NAME
+    inline get() = DisplayNameComponent.getComponentType()
+val MOVEMENT_STATES
+    inline get() = MovementStatesComponent.getComponentType()!!
+val KNOCKBACK
+    inline get() = KnockbackComponent.getComponentType()!!
+val VELOCITY
+    inline get() = Velocity.getComponentType()
+val PHYSICS_VALUES
+    inline get() = PhysicsValues.getComponentType()
+val INVULNERABLE
+    inline get() = Invulnerable.getComponentType()!!
