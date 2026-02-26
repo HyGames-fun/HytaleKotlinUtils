50e687829081bf1569ac944e48585e860b822f3a
Lion <LionPa2009@gmail.com>
2026-02-26 23:26:10 +0700

    Update CodeInitializerUtil.kt
    Update CodeInitializerUtil.kt
    
    Update CodeInitializerUtil.kt

--- src/main/java/fun/hygames/kotlinutils/codeInitialization/CodeInitializerUtil.kt
+++ src/main/java/fun/hygames/kotlinutils/codeInitialization/CodeInitializerUtil.kt
@@ -1,15 +1,28 @@
 package `fun`.hygames.kotlinutils.codeInitialization
 
+import `fun`.hygames.kotlinutils.HytaleKotlinUtils
 import java.lang.reflect.Method
 import java.lang.reflect.Modifier
+import java.util.Arrays
+import java.util.logging.Level
+import kotlin.reflect.full.createInstance
 
 object CodeInitializerUtil {
 
-    fun Method.ktInvoke(vararg args: Any?) : Any{
-        if (Modifier.isStatic(this.modifiers)) {
-            return invoke(null, *args)
-        } else {
-            return invoke(declaringClass.kotlin.objectInstance, *args)
+    fun Method.ktInvoke(vararg args: Any?) : Any {
+        return try {
+            if (Modifier.isStatic(this.modifiers)) {
+                invoke(null, *args)
+            } else {
+                if (declaringClass.kotlin.objectInstance == null) {
+                    throw NullPointerException("Object instance of ${declaringClass.kotlin.simpleName} is null!")
+                }
+                invoke(declaringClass.kotlin.objectInstance, *args)
+            }
+        } catch (exception: Exception){
+            HytaleKotlinUtils.logger.at(Level.SEVERE).log(exception.message + ". Arguments: ${args.contentToString()}")
+            exception.printStackTrace()
+            throw exception
         }
     }
 
