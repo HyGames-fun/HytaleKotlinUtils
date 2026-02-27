f866385c3cd12462e472f3ebba0ec9bcb352c918
Lion <LionPa2009@gmail.com>
2026-02-27 08:49:33 +0700

    Update CodeInitializerUtil.kt

--- src/main/java/fun/hygames/kotlinutils/codeInitialization/CodeInitializerUtil.kt
+++ src/main/java/fun/hygames/kotlinutils/codeInitialization/CodeInitializerUtil.kt
@@ -1,32 +1,25 @@
 package `fun`.hygames.kotlinutils.codeInitialization
 
 import `fun`.hygames.kotlinutils.HytaleKotlinUtils
+import java.lang.invoke.MethodHandle
 import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 import java.util.Arrays
 import java.util.logging.Level
 import kotlin.reflect.full.createInstance
+import kotlin.reflect.jvm.kotlinFunction
 
 object CodeInitializerUtil {
 
-    fun Method.ktInvoke(vararg args: Any?) : Any {
-        return try {
-            if (Modifier.isStatic(this.modifiers)) {
-                invoke(null, *args)
-            } else {
-                if (declaringClass.kotlin.objectInstance == null) {
-                    throw NullPointerException("Object instance of ${declaringClass.kotlin.simpleName} is null!")
-                }
-                invoke(declaringClass.kotlin.objectInstance, *args)
-            }
-        } catch (exception: Exception){
-            HytaleKotlinUtils.logger.at(Level.SEVERE).log(exception.message + ". Arguments: ${args.contentToString()}")
-            exception.printStackTrace()
-            throw exception
+    fun Method.ktInvoke(vararg args: Any?) : Any? {
+        if (Modifier.isStatic(this.modifiers)) {
+            return invoke(null, *args)
+        } else {
+            return invoke(declaringClass.kotlin.objectInstance, *args)
         }
     }
 
-    fun Method.ktInvoke() : Any{
+    fun Method.ktInvoke() : Any? {
         if (Modifier.isStatic(this.modifiers)) {
             return invoke(null)
         } else {
