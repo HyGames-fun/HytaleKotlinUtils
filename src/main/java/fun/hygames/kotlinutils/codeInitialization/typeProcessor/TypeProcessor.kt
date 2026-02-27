19ac34b70b8760e2a345039a4489d363518d2178
Lion <LionPa2009@gmail.com>
2026-02-27 08:50:22 +0700

    Update TypeProcessor.kt

--- src/main/java/fun/hygames/kotlinutils/codeInitialization/typeProcessor/TypeProcessor.kt
+++ src/main/java/fun/hygames/kotlinutils/codeInitialization/typeProcessor/TypeProcessor.kt
@@ -1,7 +1,6 @@
 package `fun`.hygames.kotlinutils.codeInitialization.typeProcessor
 
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
-import com.sun.source.util.Plugin
 import `fun`.hygames.kotlinutils.codeInitialization.Register
 
 interface TypeProcessor {
